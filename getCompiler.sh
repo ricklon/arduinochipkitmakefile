77946a9203040957dbb0e9c24f6aeb25fcefc7cd
Rick Anderson <rick.rickanderson@gmail.com>
2013-12-22 19:42:37 -0500

    First set of updates for chipKIT core building.

---
+++ getCompiler.sh
@@ -0,0 +1,6 @@
+#
+# Get pic32 compiler
+#
+
+curl -o ./tmp/pic32.zip -LO http://chipkit.s3.amazonaws.com/compilers/pic32-tools-chipKIT-cxx-master-arm-linux-image.zip
+unzip ./tmp/pic32.zip -d ./compiler
