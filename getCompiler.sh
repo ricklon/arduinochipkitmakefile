1259d7a5353a5c9d4fda6b5747ce1d06e72b9218
Rick Anderson <rick.rickanderson@gmail.com>
2013-12-22 22:48:53 -0500

    Got the compile process started. Also, added a print- var function. Be wary of directories with a space at the end of their name.

--- getCompiler.sh
+++ getCompiler.sh
@@ -3,4 +3,4 @@
 #
 
 curl -o ./tmp/pic32.zip -LO http://chipkit.s3.amazonaws.com/compilers/pic32-tools-chipKIT-cxx-master-arm-linux-image.zip
-unzip ./tmp/pic32.zip -d ./compiler
+unzip ./tmp/pic32.zip -d ./hardware/pic32/compiler
---
+++ getCore.sh
@@ -0,0 +1,5 @@
+#
+# Get the chipKIT core files and store in the hardware folder.
+#
+mkdir hardware
+cp -r  ~/projects/chipKIT32-MAX/hardware/pic32 ./hardware
