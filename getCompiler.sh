0f1a55271348a4a306142a2f021f4be8003306c3
Rick Anderson <rick.rickanderson@gmail.com>
2014-05-02 19:22:27 -0400

    updated compiler to mz

--- getCompiler.sh
+++ getCompiler.sh
@@ -2,5 +2,6 @@
 # Get pic33 compiler
 #
 
-curl -o ./tmp/pic32.zip -LO http://chipkit.s3.amazonaws.com/compilers/pic32-tools-chipKIT-cxx-master-arm-linux-image.zip
+#curl -o ./tmp/pic32.zip -LO http://chipkit.s3.amazonaws.com/compilers/pic32-tools-chipKIT-cxx-master-arm-linux-image.zip
+curl -o ./tmp/pic32.zip -LO http://chipkit.s3.amazonaws.com/compilers/mz/pic32-tools-chipKIT-cxx-dev_pic32mz_B-Darwin-image.zip 
 unzip ./tmp/pic32.zip -d ./hardware/pic32/compiler
